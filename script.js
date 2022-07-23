2d09b6299da86c5ff06371f5881964847e1ea715
Isaac Hussain <isaaclearns58@gmail.com>
2022-07-23 14:55:08 +0300

    s: After one day break

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const container = document.querySelector(".container");
-const button = document.querySelector("button");
+
 
 function makeSketchPad() {
     const body = document.createElement("div");
     body.setAttribute("class", "sketch-pad")
     container.appendChild(body);
-    console.log('the div loaded');
 }
 
 window.onload = makeSketchPad();
