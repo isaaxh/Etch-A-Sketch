fb4bb661b0e1c49f49962969cf1716d6d2e86233
Isaac Hussain <isaaclearns58@gmail.com>
2022-07-23 16:18:26 +0300

    Adds options UI

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-const container = document.querySelector(".container");
+// const container = document.querySelector(".container");
+// const secondContainer = document.querySelector(".secondary-Container");
 
+// function createADiv(divName,attName) {
+//     divName = document.createElement("div");
+//     divName.setAttribute("class", attName);
+//     secondContainer.appendChild(divName);
+// }
 
-function makeSketchPad() {
-    const body = document.createElement("div");
-    body.setAttribute("class", "sketch-pad")
-    container.appendChild(body);
-}
+// function createAButton(btnName, btnAttName) {
+//     btnName = document.createElement("button");
+//     btnName.innerText = btnAttName;
+//     btnName.setAttribute("class", btnAttName);
+//     optionsContainer.appendChild(btnName);
+// }
 
-window.onload = makeSketchPad();
+// let optionsContainer = 0;
+// optContAttribute = 'optionsContainer';
+
+// window.onload = createADiv(optionsContainer, optContAttribute)
+
+// sketchPadAttribute = 'sketchPad';
+// let sketchPad = 0;
+
+// window.onload = createADiv(sketchPad, sketchPadAttribute);
+
+// let colorPicker = 0;
+// colorPickerAttribute = 'colorPicker';
+// window.onload = createAButton(colorPicker, colorPickerAttribute)
