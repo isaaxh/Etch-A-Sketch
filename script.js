379b57e755a15b06b7e584963784d77a07d97b0b
Isaac Hussain <isaaclearns58@gmail.com>
2022-07-24 11:55:35 +0300

    Adds hover effect to divs inside the pad

--- script.js
+++ script.js
@@ -2,34 +2,48 @@ const sketchPad = document.querySelector('.sketchPad');
 const btnSetSize = document.querySelector('#btnSetSize');
 const btnReset = document.querySelector('#btnReset');
 
-function populateSketchPad(size) {
+
+function populatePad(size) {
     sketchPad.style.gridTemplateColumns = `repeat(${size}, 1fr)`
     sketchPad.style.gridTemplateColumns = `repeat(${size}, 1fr)`
     
+    //const marker = 'black'
+    
     for(let i = 0; i < (size * size); i++) {  
-        let square = document.createElement('div');
-        square.style.backgroundColor = 'red';
+        const square = document.createElement('div');
+        square.classList.add('divsInsidePad');
+        //square.addEventListener('mousedown', markingThePad(marker, square))
+        square.addEventListener('mouseover', () => {
+             square.style.backgroundColor = 'black';
+        })
+        
         sketchPad.insertAdjacentElement('beforeend',square);    
     }
 }
 
-function sketchPadSizeValidations(size) {
-    
+function markingThePad(color, targetDiv) {
+    console.log('this works')
+    targetDiv.style.backgroundColor = color;
+}
+
+function sizeValidation(size) {  
     if (size <= 0 || size > 64) {
         return console.log('Please enter a value between 0 and 64');
     } else {
-        populateSketchPad(size);
+        sketchPad.textContent = '';
+        populatePad(size);
     }
 }
 
-
-
 btnSetSize.addEventListener('click', () => {
     const sketchPadSize = document.querySelector('#sizeInput').value;
-    sketchPadSizeValidations(sketchPadSize);
+    sizeValidation(sketchPadSize);
 })
-    
+
 btnReset.addEventListener('click', () => {
     sketchPad.textContent = '';
 })    
-    
+
+//populatePad(sketchPadSize);
+//const sketchPadSize = document.querySelector('#sizeInput').value
+//window.onload = populatePad(sketchPadSize)
